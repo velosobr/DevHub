9ec62d7ec32e1a034987a2b5e5b0c96a1956c763
Lino Veloso <lino.veloso@ifood.com.br>
2022-06-11 23:14:45 -0300

    Added the challenge Code

--- app/src/main/java/com/ragnlabs/devhub/MainActivity.kt
+++ app/src/main/java/com/ragnlabs/devhub/MainActivity.kt
@@ -3,12 +3,14 @@ package com.ragnlabs.devhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.ragnlabs.devhub.ui.theme.DevHubTheme
 
@@ -17,12 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DevHubTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GreetingChallenge(
+                        name = "Lino Veloso",
+                        githubUser = "velosobr",
+                        bioContent = "Software Engineer at @ifood. " +
+                                "Bachelor in Informational Systems at @UFSC."
+                    )
                 }
             }
         }
@@ -30,14 +36,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingChallenge(name: String, githubUser: String, bioContent: String) {
+
+    Image(R.drawable.ic_launcher_background)
+    Column {
+        Text(text = name)
+        Text(text = githubUser)
+        Text(text = bioContent)
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DevHubTheme {
-        Greeting("Android")
+        GreetingChallenge(
+            name = "Lino Veloso",
+            githubUser = "velosobr",
+            bioContent = "Software Engineer at @ifood. Bachelor in Informational Systems at @UFSC."
+        )
     }
 }
